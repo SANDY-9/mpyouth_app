3b8ba27016ceb81028541ebb9c81bf73f77eea07
SANDY-9 <nnal0256@naver.com>
2021-09-08 12:58:32 +0900

    0908

--- MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/MainActivity.kt
+++ MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package go.kr.mapo.mapoyouth.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import go.kr.mapo.mapoyouth.R
 import go.kr.mapo.mapoyouth.databinding.ActivityMainBinding
 
@@ -11,8 +13,10 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        binding = ActivityMainBinding.inflate(layoutInflater).apply { setContentView(root) }
+        with(binding) {
+            val navController = findNavController(R.id.navHostFragment)
+            navBottom.setupWithNavController(navController)
+        }
     }
 }

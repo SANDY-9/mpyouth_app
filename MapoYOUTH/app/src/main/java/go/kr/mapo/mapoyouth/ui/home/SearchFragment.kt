680bb53a267fd746a829066173796ade54068554
SeulgiLim <71672106+SeulgiLim@users.noreply.github.com>
2021-09-09 17:44:19 +0900

    210909 fragment_setting.xml

---
+++ MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/home/SearchFragment.kt
@@ -0,0 +1,21 @@
+package go.kr.mapo.mapoyouth.ui.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import go.kr.mapo.mapoyouth.R
+
+class SearchFragment: Fragment(R.layout.fragment_search){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/home/SettingFragment.kt
@@ -0,0 +1,12 @@
+package go.kr.mapo.mapoyouth.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import go.kr.mapo.mapoyouth.R
+
+class SettingFragment: Fragment(R.layout.fragment_setting) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}

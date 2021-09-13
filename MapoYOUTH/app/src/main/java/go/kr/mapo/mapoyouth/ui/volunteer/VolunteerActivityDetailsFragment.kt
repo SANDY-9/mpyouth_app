92f8804099cc41d16faafa4604362f7bd7ffebfe
SANDY-9 <nnal0256@naver.com>
2021-09-13 16:08:10 +0900

    0613

---
+++ MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/volunteer/VolunteerActivityDetailsFragment.kt
@@ -0,0 +1,22 @@
+package go.kr.mapo.mapoyouth.ui.volunteer
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import go.kr.mapo.mapoyouth.R
+class VolunteerActivityDetailsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_volunteer_activity_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
--- MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/volunteer/VolunteerListAdapter.kt
+++ MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/volunteer/VolunteerListAdapter.kt
@@ -1,10 +1,12 @@
 package go.kr.mapo.mapoyouth.ui.volunteer
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import go.kr.mapo.mapoyouth.R
+import go.kr.mapo.mapoyouth.ui.youth.YouthDetailsActivity
 
 /**
  * @author SANDY
@@ -15,14 +17,18 @@ import go.kr.mapo.mapoyouth.R
 class VolunteerListAdapter(val list: List<String>) : RecyclerView.Adapter<VolunteerListAdapter.HolderView>() {
 
     inner class HolderView(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        fun setBinding() {
+            itemView.setOnClickListener {
+                it.context.startActivity(Intent(it.context, VolunteerDetailsActivity::class.java))
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HolderView =
         HolderView(LayoutInflater.from(parent.context).inflate(R.layout.item_volunteer_rv, parent, false))
 
     override fun onBindViewHolder(holder: HolderView, position: Int) {
-
+        holder.setBinding()
     }
 
     override fun getItemCount(): Int = list.size

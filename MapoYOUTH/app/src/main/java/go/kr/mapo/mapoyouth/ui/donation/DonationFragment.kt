8aec6da68d70526f611f9b8c13c5238c5f0fff65
hankim <42214405+byhankim@users.noreply.github.com>
2021-09-14 11:40:50 +0900

    app/src/main/

--- MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/donation/DonationFragment.kt
+++ MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/donation/DonationFragment.kt
@@ -5,56 +5,66 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import go.kr.mapo.mapoyouth.R
+import go.kr.mapo.mapoyouth.databinding.FragmentDonationBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DonationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DonationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var donationView: View
+
+    private lateinit var rv: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donation, container, false)
+        donationView = inflater.inflate(R.layout.fragment_donation, container, false)
+        rv = donationView.findViewById(R.id.donationRv)
+
+        with (rv) {
+            this.layoutManager = LinearLayoutManager(donationView.context)
+            this.adapter = DonationRecyclerViewAdapter(getDummyData())
+        }
+
+        return donationView
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DonationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DonationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        val manager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+//
+//        with (binding.donationRv) {
+//            layoutManager = manager
+//            addItemDecoration(DividerItemDecoration(this.context,
+//            LinearLayoutManager.VERTICAL))
+//            adapter = DonationRecyclerViewAdapter(getDummyData())
+//        }
+    }
+
+
+    private fun getDummyData(): MutableList<String> {
+        val dummyData = mutableListOf<String>()
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+        dummyData.add("초,중학교 학습지원 희망해요! (전교과 가능)")
+
+        return dummyData
     }
 }

9413852353a25e9b75b27adc79770c27a63fd9d4
yforyuri <yforyurirho@gmail.com>
2021-09-16 17:21:59 +0900

    donation fragment rough layout

--- MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/donation/DonationFragment.kt
+++ MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/donation/DonationFragment.kt
@@ -5,16 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import go.kr.mapo.mapoyouth.R
+import go.kr.mapo.mapoyouth.databinding.FragmentDonationBinding
 
 class DonationFragment : Fragment() {
+
+    lateinit var binding :FragmentDonationBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_donation, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_donation, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        binding.donationRv.apply {
+            this.adapter = DonationRecyclerViewAdapter(listOf("1","2","3","4","5","6","7","8"))
+            this.layoutManager = LinearLayoutManager(binding.root.context, LinearLayoutManager.VERTICAL,false)
+        }
     }
 
 }

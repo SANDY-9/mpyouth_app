9d3004470672c5d4a116676c95b74b5bd94accdb
hankim <42214405+byhankim@users.noreply.github.com>
2021-09-28 10:22:38 +0900

    Onboarding Screens skip btn fix

--- MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/onboard/screens/OnboardSecondScreen.kt
+++ MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/onboard/screens/OnboardSecondScreen.kt
@@ -1,15 +1,28 @@
 package go.kr.mapo.mapoyouth.ui.onboard.screens
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import androidx.viewpager2.widget.ViewPager2
 import go.kr.mapo.mapoyouth.R
 import go.kr.mapo.mapoyouth.databinding.FragmentOnboardSecondScreenBinding
+import go.kr.mapo.mapoyouth.ui.MainActivity
+import go.kr.mapo.mapoyouth.util.ONBOARD_FINISHED_STR
+import go.kr.mapo.mapoyouth.util.ONBOARD_SHARED_PREF
 
 class OnboardSecondScreen : Fragment() {
+
+    private lateinit var owner : MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        owner = context as MainActivity
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -17,13 +30,20 @@ class OnboardSecondScreen : Fragment() {
         // Inflate the layout for this fragment
         val binding = FragmentOnboardSecondScreenBinding.inflate(inflater, container, false)
 
-        val vp = activity?.findViewById<ViewPager2>(R.id.onboardingViewPager)
-
         binding.btnSkipScreen02.setOnClickListener {
-            vp?.currentItem = 2
+            findNavController().navigate(R.id.action_onboardingFragment_to_homeFragment)
+            onBoardingFinished()
+            owner.binding.navBottom.visibility = View.VISIBLE
         }
 
         return binding.root
+    }
+
+    private fun onBoardingFinished() {
+        val sharedPref = requireActivity().getSharedPreferences(ONBOARD_SHARED_PREF, Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean(ONBOARD_FINISHED_STR, true)
+        editor.apply()
     }
 
 }

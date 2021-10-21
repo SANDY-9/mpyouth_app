176119e64c47c0485f5084e6d17caefb2e482a0c
SANDY-9 <nnal0256@naver.com>
2021-10-21 20:53:54 +0900

    1021 검색 디버깅
    증상 : 현재 필터된 카테고리에서 검색 성공 후 상세페이지에 들어간뒤 다시 돌아오면 필터가 되지 않고 리셋되는 현상
    원인 : Fragment가 onPause상태가 되고(중단됐던 observer) 다시 Start되면서 중단댔던 옵저버가 리셋되었기 때문
    해결 : 처음부터 lawData를 직접 주지않고, 전역변수로 현재 카테고리를 저장한 뒤 데이터를 카테고리 전역변수에 따라 필터링하여 어댑터에 전달

--- MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/donation/DonationFragment.kt
+++ MapoYOUTH/app/src/main/java/go/kr/mapo/mapoyouth/ui/donation/DonationFragment.kt
@@ -1,17 +1,21 @@
 package go.kr.mapo.mapoyouth.ui.donation
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
 import android.widget.RadioButton
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
+import androidx.paging.PagingData
 import androidx.paging.filter
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import go.kr.mapo.mapoyouth.R
 import go.kr.mapo.mapoyouth.databinding.FragmentDonationBinding
+import go.kr.mapo.mapoyouth.network.response.DonationListResponse
+import go.kr.mapo.mapoyouth.ui.search.SearchActivity
 
 @AndroidEntryPoint
 class DonationFragment : Fragment() {
@@ -34,8 +38,22 @@ class DonationFragment : Fragment() {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupToolbar()
         getDonationList()
         getDonationFilterList()
+    }
+
+    private fun setupToolbar() = binding.toolbar.apply {
+        setOnMenuItemClickListener {
+            when(it.itemId) {
+                R.id.menu_search -> {
+                    startActivity(Intent(requireContext(), SearchActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
     }
 
     private fun getDonationList() {
@@ -45,6 +63,8 @@ class DonationFragment : Fragment() {
                 LinearLayoutManager(binding.root.context, LinearLayoutManager.VERTICAL, false)
         }
     }
+
+    private var curCategory = "전체"
 
     @SuppressLint("ResourceType")
     private fun getDonationFilterList() {
@@ -61,7 +81,8 @@ class DonationFragment : Fragment() {
 
         viewModel.donationList.observe(viewLifecycleOwner, {
 
-            var category: String? = null
+            //var category: String? = null
+            /*
             when(category) {
                 "전체" -> 0
                 "디자인" -> 1
@@ -72,18 +93,19 @@ class DonationFragment : Fragment() {
                 "문서작성" -> 6
                 else -> 7
             }
-            val data = it
-            donationAdapter.submitData(lifecycle, data)
+             */
+            //val data = it
+            donationAdapter.submitData(lifecycle, getData(it))
             for (i in 0..7) {
                 categoryArray[i].setOnCheckedChangeListener { button, isChecked ->
                     if(isChecked) {
-                        var filterData = data
+                        //var filterData = data
+                        //if( i > 0 ) filterData = data.filter { it.category.name == categoryArray[i].text }
 
-                        if( i > 0 ) filterData = data.filter { it.category.name == categoryArray[i].text }
-
+                        curCategory = categoryArray[i].text.toString()
                         button.setBackgroundResource(bgColorArray[i])
                         button.setTextColor(resources.getColor(colorArray[i]))
-                        donationAdapter.submitData(lifecycle, filterData)
+                        donationAdapter.submitData(lifecycle, getData(it))
                     } else {
                         button.setBackgroundResource(R.drawable.bg_dona_btn)
                         button.setTextColor(resources.getColor(R.color.mapo_gray))
@@ -91,5 +113,10 @@ class DonationFragment : Fragment() {
                 }
             }
         })
+    }
+
+    private fun getData(data: PagingData<DonationListResponse.Data.Content>) = when(curCategory) {
+        "전체" -> data
+        else -> data.filter { it.category.name == curCategory }
     }
 }
